e96963a7929e5e14ff730b2547070083856f704b
Maverick Adolfo <merkonical@gmail.com>
2019-11-11 23:26:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,13 @@
-# def square_array(array)
-#   squared = []
-#   array.each do |element| 
-#     squared << element ** 2
-#   end 
-#   squared
-# end
-
 def square_array(array)
-  array.map do |element| 
-    element ** 2
+  squared = []
+  array.each do |element| 
+    squared << element ** 2
   end 
+  squared
 end
+
+# def square_array(array)
+#   array.map do |element| 
+#     element ** 2
+#   end 
+# end
